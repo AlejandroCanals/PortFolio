a0b3185a19955e087de18f390befabd096f85ee1
iTzSRK <Alejandrocanals17@gmail.com>
2024-02-28 17:59:08 +0100

    nuevos cambios

--- Portfolio/src/components/projects/Projects.jsx
+++ Portfolio/src/components/projects/Projects.jsx
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
 import "./projects.css";
-import GestorImg from "../../assets/GestorReparaciones.png";
-import MultitareaImg from "../../assets/AppMultitarea.png";
-import FestivalImg from '../../assets/Festival.png' 
+import GestorImg from "../../assets/Proyectos/GestorReparaciones.png";
+import MultitareaImg from "../../assets/Proyectos/AppMultitarea.png";
+import FestivalImg from "../../assets/Proyectos/Festival.png";
+import BlogCafeImg from "../../assets/Proyectos/BlogCafe.png";
 
 function Projects() {
   const [toogleState, setToogleState] = useState(0);
@@ -20,7 +21,9 @@ function Projects() {
             <h3 className="projects__title">Gestor de Reparaciones</h3>
           </div>
 
-         <a href="https://repaircrm.netlify.app/"><img className="projects__img" src={GestorImg} alt=""></img></a> 
+          <a href="https://repaircrm.netlify.app/">
+            <img className="projects__img" src={GestorImg} alt=""></img>
+          </a>
 
           <span className="projects__description" onClick={() => toggleTab(1)}>
             Descripción
@@ -103,12 +106,13 @@ function Projects() {
             <h3 className="projects__title">App Multitarea</h3>
           </div>
           <a href="https://flask-multitools.onrender.com/">
-          <img
-            className="projects__img"
-            src={MultitareaImg}
-            style={{ height: "191px" }}
-            alt=""
-          ></img></a>
+            <img
+              className="projects__img"
+              src={MultitareaImg}
+              style={{ height: "191px" }}
+              alt=""
+            ></img>
+          </a>
 
           <span className="projects__description" onClick={() => toggleTab(2)}>
             Descripción
@@ -174,13 +178,13 @@ function Projects() {
             </div>
           </div>
         </div>
-        
+
         <div className="projects__content">
           <div>
-            <h3 className="projects__title">Gestor de Reparaciones3</h3>
+            <h3 className="projects__title">Web Festival</h3>
           </div>
           <a href="https://music-festivaal.netlify.app">
-          <img className="projects__img" src={FestivalImg} alt=""></img>
+            <img className="projects__img" src={FestivalImg} alt=""></img>
           </a>
 
           <span className="projects__description" onClick={() => toggleTab(3)}>
@@ -200,19 +204,15 @@ function Projects() {
                 onClick={() => toggleTab(0)}
                 className="uil uil-times projects__modal-close"
               ></i>
-              <h3 className="projects__modal-title">Gestor de Reparaciones3</h3>
+              <h3 className="projects__modal-title">Web Festival</h3>
               <p className="projects__modal-description">
-                💡 Esta app surge de mi experiencia como técnico en reparaciones
-                electrónicas donde las incidencias se trabajaban con excel y en
-                mi opinión no cubrían las necesidades del trabajo.
-                <br />
-                <br />
-                👨‍💻 Es un gestor que permite crear, leer, actualizar y eliminar
-                informes de incidencias, además de asignar técnicos a cada
-                reparación.
-                <br />
-                <br />
-                🔧 Incluye un seguimiento automatizado de las mismas.
+                Página web de un festival que ofrece entradas para sus
+                conciertos. El sitio está construido principalmente con HTML,
+                CSS y JavaScript para lograr estructura, estilo e
+                interactividad.
+                <br></br>
+                <br></br> Además, se implementa Sass (SCSS) para una gestión de
+                estilo más eficiente y organizada.
               </p>
 
               <h3>Tecnologías Usadas</h3>
@@ -225,23 +225,23 @@ function Projects() {
 
                 <li className="projects__modal-service">
                   <i className="uil uil-check-circle projects__modal-icon"></i>
-                  <p className="projects__modal-info">CSS</p>
+                  <p className="projects__modal-info">SCSS</p>
                 </li>
 
                 <li className="projects__modal-service">
                   <i className="uil uil-check-circle projects__modal-icon"></i>
-                  <p className="projects__modal-info">Tailwind</p>
+                  <p className="projects__modal-info">JavaScript</p>
                 </li>
 
                 <li className="projects__modal-service">
                   <i className="uil uil-check-circle projects__modal-icon"></i>
-                  <p className="projects__modal-info">React</p>
+                  <p className="projects__modal-info">Node.js</p>
                 </li>
                 <li className="projects__modal-service">
                   <i className="uil uil-check-circle projects__modal-icon"></i>
-                  <p className="projects__modal-info">Python y Django</p>
+                  <p className="projects__modal-info">Gulp</p>
                 </li>
-                <a href="https://github.com/">
+                <a href="https://github.com/AlejandroCanals/Festival-Project">
                   <li className="projects__modal-service">
                     <i
                       class="uil uil-github"
@@ -254,31 +254,26 @@ function Projects() {
             </div>
           </div>
         </div>
-
-        
       </div>
 
-      
       <div className="projects__container container grid">
         <div className="projects__content">
           <div>
-            <h3 className="projects__title">Gestor de Reparaciones</h3>
+            <h3 className="projects__title">Blog de Café</h3>
           </div>
 
-         <a href="https://repaircrm.netlify.app/"><img className="projects__img" src={GestorImg} alt=""></img></a> 
+          <a href="https://articulosdecafe.netlify.app/">
+            <img className="projects__img" src={BlogCafeImg} alt=""></img>
+          </a>
 
-          <span className="projects__description" onClick={() => toggleTab(1)}>
+          <span className="projects__description" onClick={() => toggleTab(4)}>
             Descripción
             <i className="uil uil-arrow-right projects__button-icon"></i>
           </span>
-          <div>
-            <a href="https://www.linkedin.com/feed/update/urn:li:activity:7150441143128731649/">
-              Ver video
-            </a>
-          </div>
+
           <div
             className={
-              toogleState === 1
+              toogleState === 4
                 ? "projects__modal active-modal"
                 : "projects__modal"
             }
@@ -288,19 +283,19 @@ function Projects() {
                 onClick={() => toggleTab(0)}
                 className="uil uil-times projects__modal-close"
               ></i>
-              <h3 className="projects__modal-title">Gestor de Reparaciones1</h3>
+              <h3 className="projects__modal-title">Blog Cafe</h3>
               <p className="projects__modal-description">
-                💡 Esta app surge de mi experiencia como técnico en reparaciones
-                electrónicas donde las incidencias se trabajaban con excel y en
-                mi opinión no cubrían las necesidades del trabajo.
-                <br />
-                <br />
-                👨‍💻 Es un gestor que permite crear, leer, actualizar y eliminar
-                informes de incidencias, además de asignar técnicos a cada
-                reparación.
-                <br />
-                <br />
-                🔧 Incluye un seguimiento automatizado de las mismas.
+                Este proyecto es un blog de café creado como parte de mi
+                aprendizaje en desarrollo web.
+                <br></br>
+                <br></br>
+                Fue uno de mis primeros proyectos y lo desarrollé para practicar
+                mis habilidades en HTML, CSS y JavaScript.
+                <br></br>
+                <br></br>
+                El sitio web presenta un diseño totalmente responsive para
+                adaptarse a dispositivos móviles, tabletas y computadoras de
+                escritorio. café.
               </p>
 
               <h3>Tecnologías Usadas</h3>
@@ -318,18 +313,10 @@ function Projects() {
 
                 <li className="projects__modal-service">
                   <i className="uil uil-check-circle projects__modal-icon"></i>
-                  <p className="projects__modal-info">Tailwind</p>
+                  <p className="projects__modal-info">JavaScript</p>
                 </li>
 
-                <li className="projects__modal-service">
-                  <i className="uil uil-check-circle projects__modal-icon"></i>
-                  <p className="projects__modal-info">React</p>
-                </li>
-                <li className="projects__modal-service">
-                  <i className="uil uil-check-circle projects__modal-icon"></i>
-                  <p className="projects__modal-info">Python y Django</p>
-                </li>
-                <a href="https://github.com/AlejandroCanals/RepairShop-CRM">
+                <a href="https://github.com/AlejandroCanals/CafeBlog-Project">
                   <li className="projects__modal-service">
                     <i
                       class="uil uil-github"
@@ -348,14 +335,15 @@ function Projects() {
             <h3 className="projects__title">App Multitarea</h3>
           </div>
           <a href="https://flask-multitools.onrender.com/">
-          <img
-            className="projects__img"
-            src={MultitareaImg}
-            style={{ height: "191px" }}
-            alt=""
-          ></img></a>
+            <img
+              className="projects__img"
+              src={MultitareaImg}
+              style={{ height: "191px" }}
+              alt=""
+            ></img>
+          </a>
 
-          <span className="projects__description" onClick={() => toggleTab(2)}>
+          <span className="projects__description" onClick={() => toggleTab(5)}>
             Descripción
             <i className="uil uil-arrow-right projects__button-icon"></i>
           </span>
@@ -367,7 +355,7 @@ function Projects() {
 
           <div
             className={
-              toogleState === 2
+              toogleState === 5
                 ? "projects__modal active-modal"
                 : "projects__modal"
             }
@@ -419,23 +407,23 @@ function Projects() {
             </div>
           </div>
         </div>
-        
+
         <div className="projects__content">
           <div>
             <h3 className="projects__title">Gestor de Reparaciones3</h3>
           </div>
           <a href="https://music-festivaal.netlify.app">
-          <img className="projects__img" src={FestivalImg} alt=""></img>
+            <img className="projects__img" src={FestivalImg} alt=""></img>
           </a>
 
-          <span className="projects__description" onClick={() => toggleTab(3)}>
+          <span className="projects__description" onClick={() => toggleTab(6)}>
             Descripción
             <i className="uil uil-arrow-right projects__button-icon"></i>
           </span>
 
           <div
             className={
-              toogleState === 3
+              toogleState === 6
                 ? "projects__modal active-modal"
                 : "projects__modal"
             }
@@ -499,12 +487,7 @@ function Projects() {
             </div>
           </div>
         </div>
-
-        
       </div>
-
-
-      
     </section>
   );
 }
